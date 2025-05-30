cf812805cd337bc19d8255b5db6dabe81a877134
dsahu9555 <91771445+dsahu9555@users.noreply.github.com>
2025-05-30 09:05:28 +0530

    Create Task2.py

---
+++ Assignment1/Task2.py
@@ -0,0 +1,4 @@
+first_name = input("Enter the first name: ")
+last_name = input("Enter the last name: ")
+print("\n")
+print("Hello, " + first_name + " " + last_name + "! Welcome to the Python program.")
