f73bb3d06c0077227dee840dd839f738c800883c
dsahu9555 <91771445+dsahu9555@users.noreply.github.com>
2025-05-30 16:42:15 +0530

    Create task2.py

---
+++ Assignment5/task2.py
@@ -0,0 +1,6 @@
+list1 = [1,2,3,4,5,6,7,8,9,10]
+print("Original list: {}".format(list1))
+list2 = list1[:5]
+print("Extracted first five elements: {}".format(list2))
+list3 = list2[::-1]
+print("Reversed extracted elements: {}".format(list3))
